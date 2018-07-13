a97c219eb3544ff2dccec9a1311a274ddac54f4a
B_plus_tree <37645301+xihao1234567@users.noreply.github.com>
2018-07-13 16:14:50 +0800

    Update B_PLUS_tree.h

--- B_PLUS_tree.h
+++ B_PLUS_tree.h
@@ -1,44 +1,49 @@
 #ifndef BPlusTree_h
 #define BPlusTree_h
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
 
-#define M 4   //定义B+树的阶数
-#define LIMIT_M_2 (M%2==1 ? (M+1)/2:M/2)  //最小的键值数
+#define M (4)
+
+#define LIMIT_M_2 (M % 2 ? (M + 1)/2 : M/2)
+
 typedef struct BPlusNode *BPlusTree,*Position;
-typedef int keyType;
+typedef int KeyType;
 struct BPlusNode{
-    int keyNum;
-    keyType key[M+1];
-    BPlusTree Children[M+1];
+
+    int KeyNum;
+
+    KeyType Key[M + 1];
+
+    BPlusTree Children[M + 1];
+
     BPlusTree Next;
+
 };
 
-/*定义树的操作*/
+ 
 
-/* 初始化*/
-BPlusTree Initialize();//返回一个树的根节点
+/* 初始化 */
 
-/* 插入*/
+extern BPlusTree Initialize();
+
+/* 插入 */
+
 extern BPlusTree Insert(BPlusTree T,KeyType Key);
 
 /* 删除 */
+
 extern BPlusTree Remove(BPlusTree T,KeyType Key);
 
 /* 销毁 */
+
 extern BPlusTree Destroy(BPlusTree T);
 
 /* 遍历节点 */
+
 extern void Travel(BPlusTree T);
 
-/* 遍历树叶节点的数据 */
-extern void TravelData(BPlusTree T);
-
- 
-#endif /* BPlusTree_h */
 
 
-
-
+#endif
